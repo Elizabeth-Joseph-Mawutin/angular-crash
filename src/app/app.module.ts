0f3592f041ff6c0c37269766274d9bed8c7f2b5d
Elizabeth Joseph <lizzy22.mj@gmail.com>
2023-02-24 18:17:53 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+import { TasksComponent } from './compnents/tasks/tasks.component';
+import { TasksItemComponent } from './components/task-item/task-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    TasksComponent,
+    TasksItemComponent
   ],
   imports: [
     BrowserModule
